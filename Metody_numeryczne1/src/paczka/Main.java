8cd96f26f38ad565e3b123196109168e2c98a950
Adam <niewpisze@gmail.com>
2011-10-15 18:20:17 +0200

    przejscie na BigDecimal

--- Metody_numeryczne1/src/paczka/Main.java
+++ Metody_numeryczne1/src/paczka/Main.java
@@ -3,6 +3,7 @@ package paczka;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 /*Napisać program realizujący różne metody znajdowania przybliżonego rozwiązania równania 2x-tanx = 0 (Chodzi o rozwiązanie x!=0)
  * Należy rozważać następujące algorytmy:
@@ -23,20 +24,27 @@ public class Main {
 		System.out.println("6 - zakończ");
 	}
 
-	static int[] pobierzPrzedzial() throws NumberFormatException, IOException {
-		int[] przedzial = { -3, 3 }; // TODO potem zmienic -3 na 4
+	static BigDecimal[] pobierzWarunki() throws NumberFormatException,
+			IOException {
+		BigDecimal[] warunki = new BigDecimal[3];
+		warunki[0] = BigDecimal.valueOf(4);// TODO potem zmienic -2 na 4
+		warunki[1] = BigDecimal.valueOf(2);
 		BufferedReader klawa = new BufferedReader(new InputStreamReader(
 				System.in));
-		// while (przedzial[0] > przedzial[1]) {
-		// System.out.println("Podaj początek przedziału:");
-		// przedzial[0] = Integer.parseInt(klawa.readLine());
-		// System.out.println("Podaj koniec przedziału: ");
-		// przedzial[1] = Integer.parseInt(klawa.readLine());
-		// if (przedzial[0] > przedzial[1]) {
-		// System.out.println("Błąd");
-		// }
-		// }
-		return przedzial;
+		while (warunki[0].compareTo(warunki[1]) == 0
+				|| warunki[0].compareTo(warunki[1]) == 1) {
+			System.out.println("Podaj początek przedziału:");
+			warunki[0] = new BigDecimal((klawa.readLine()));
+			System.out.println("Podaj koniec przedziału: ");
+			warunki[1] = new BigDecimal((klawa.readLine()));
+			System.out.println("Podaj precyzje zaookrąglenia: ");
+			warunki[2] = new BigDecimal((klawa.readLine()));
+			if (warunki[0].compareTo(warunki[1]) == 0
+					|| warunki[0].compareTo(warunki[1]) == 1) {
+				System.out.println("Błąd");
+			}
+		}
+		return warunki;
 	}
 
 	/**
@@ -52,7 +60,7 @@ public class Main {
 		}
 		boolean menu = false;
 		int option = 1;
-		int[] zakres = new int[2];
+		BigDecimal[] warunki = new BigDecimal[3];
 		BufferedReader klaw = new BufferedReader(new InputStreamReader(
 				System.in));
 		do {
@@ -67,9 +75,9 @@ public class Main {
 				drukujMenu();
 				break;
 			case 2:
-				zakres = pobierzPrzedzial();
-				PoloweniePrzedzialow polo = new PoloweniePrzedzialow(zakres[0],
-						zakres[1], loud);
+				warunki = pobierzWarunki();
+				PoloweniePrzedzialow polo = new PoloweniePrzedzialow(
+						warunki[0], warunki[1], warunki[2], loud);
 				break;
 			case 6:
 				menu = true;
--- Metody_numeryczne1/src/paczka/PoloweniePrzedzialow.java
+++ Metody_numeryczne1/src/paczka/PoloweniePrzedzialow.java
@@ -1,19 +1,40 @@
 package paczka;
 
+import java.math.BigDecimal;
+
 public class PoloweniePrzedzialow {
-	int zakres0 = -3;
-	int zakres1 = 3;
+	BigDecimal zakres0 = null;
+	BigDecimal zakres1 = null;
+	int zaookraglenie = 10;
 	boolean loud = true;
+
+	private BigDecimal Szukaj(BigDecimal zakres0, BigDecimal zakres1,
+			int zaookraglenie, boolean loud) {
+		BigDecimal wynik = null;
+		BigDecimal middle = (zakres0.add(zakres1)).divide(new BigDecimal(2), zaookraglenie,
+				BigDecimal.ROUND_HALF_UP);
+
+		if (!loud) {
+			System.out.println("middle =" + middle.toString());
+		}
+		
+		
+
+		return wynik;
+	}
 
 	/**
 	 * @param zakres0
 	 * @param zakres1
 	 */
-	public PoloweniePrzedzialow(int zakres0, int zakres1, boolean loud) {
+	public PoloweniePrzedzialow(BigDecimal zakres0, BigDecimal zakres1,
+			BigDecimal zaookraglenie, boolean loud) {
 		super();
 		this.zakres0 = zakres0;
 		this.zakres1 = zakres1;
+		this.zaookraglenie = Integer.parseInt(zaookraglenie.toString());
 		this.loud = loud;
+		BigDecimal wynik;
 
 		if (!loud) {
 			System.out.println("zakres0 = " + this.zakres0);
@@ -22,9 +43,9 @@ public class PoloweniePrzedzialow {
 		if (!loud) {
 			System.out.println("zakres1 = " + this.zakres1);
 		}
-		
-		
-		
-		
+		if (!loud) {
+			System.out.println("zaookraglenie  =" + this.zaookraglenie);
+		}
+		Szukaj(this.zakres0, this.zakres1, this.zaookraglenie, this.loud);
 	}
 }

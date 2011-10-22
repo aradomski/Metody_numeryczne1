ea02c8fa02c474a1e36587c2d0a5e68c80af318d
Adam <niewpisze@gmail.com>
2011-10-22 22:39:39 +0200

    Newton chyba działa wyniki się zgadzają

--- Metody_numeryczne1/src/paczka/Newton.java
+++ Metody_numeryczne1/src/paczka/Newton.java
@@ -9,6 +9,11 @@ public class Newton {
 	private double iloscIteracji;
 	private double x0; // x początkowy
 
+	private double Pochodna(double x) {
+		return x = 2 - 4 * (Math.pow(Math.cos(x), 2))
+				/ Math.pow((1 + Math.cos(2 * x)), 2);
+	}
+
 	/**
 	 * @param x0
 	 * @param iloscIteracji
@@ -19,5 +24,26 @@ public class Newton {
 		this.x0 = x0;
 		this.iloscIteracji = iloscIteracji;
 		this.loud = loud;
+		Wzor wzor = new Wzor(loud);
+		double x1 = this.x0 - 1;
+		double f0 = wzor.Oblicz(x0), f1;
+		int i = (int) iloscIteracji;
+		while (i > 0 && Math.abs(x1 - this.x0) > epsX && Math.abs(f0) > eps0) {
+			f1 = Pochodna(this.x0);
+			if (Math.abs(f1) < eps0) {
+				System.out.println("Zły punkt startowy");
+				wynik = -1000;
+				break;
+			}
+			x1 = this.x0;
+			this.x0 = this.x0 - f0 / f1;
+			f0 = wzor.Oblicz(this.x0);
+			i--;
+			if (i == 0) {
+				System.out.println("Przekroczono limit obiegów");
+			}
+			wynik = this.x0;
+		}
+
 	}
 }
